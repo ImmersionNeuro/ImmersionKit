bfdaac3d185ab903039b889398bc645dc6b3ee3c
Anka <runner@sat12-dp149-eee89bcc-974a-4c7e-9336-cd572dae1835-5E379ED1489C.local>
2025-09-24 05:24:02 +0000

    chore: update Package.swift for v0.0.2 (iOS 17, checksum 4793ff8ce4daf96d28496e30c49b58656c1170366abffae74141b2fcc6e0edf4)

--- Package.swift
+++ Package.swift
@@ -1,1 +1,19 @@
-// placeholder; will be updated automatically on first release
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "ImmersionKit",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(name: "ImmersionKit", targets: ["ImmersionKit"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ImmersionKit",
+            url: "https://github.com/ImmersionNeuro/ImmersionKit/releases/download/v0.0.2/ImmersionKit.xcframework.zip",
+            checksum: "4793ff8ce4daf96d28496e30c49b58656c1170366abffae74141b2fcc6e0edf4"
+        )
+    ]
+)
