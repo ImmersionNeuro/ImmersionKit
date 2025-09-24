d2648de21bf189a01c3b580ffc436b7f28380227
Scott Brown <scott_b@me.com>
2025-09-24 16:28:31 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         .macOS(.v15)
     ],
     products: [
-        .library(name: "ImmersionKit", targets: ["ImmersionKitBinary"])
+               .library(name: "ImmersionKit", targets: ["ImmersionKit"])
     ],
     targets: [
-        .binaryTarget(
-            name: "ImmersionKitBinary",
-            url: "https://github.com/ImmersionNeuro/ImmersionKit/releases/download/v0.0.57/ImmersionKit.xcframework.zip",
-            checksum: "17d4e68de44bf7df13602ff502646a4078e92d4f0f8233c34d81e0565cd8227f"
-        )
+              .binaryTarget(
+                            name: "ImmersionKit",
+                  url: "https://github.com/ImmersionNeuro/ImmersionKit/releases/download/v0.0.57/ImmersionKit.xcframework.zip",
+                  checksum: "17d4e68de44bf7df13602ff502646a4078e92d4f0f8233c34d81e0565cd8227f"
+              )
     ]
 )
