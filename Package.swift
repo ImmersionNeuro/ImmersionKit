15234bdcf5722c6d6fb5ab62953fc7ad0c33f154
Anka <runner@Mac-1758901192403.local>
2025-09-26 15:46:19 +0000

    chore: update Package.swift for v0.0.68 (iOS 17, tvOS 17, watchOS 10, macOS 15; checksum 0ebc5d911623e9259d929bdd03b66b491e0e7a6c878fa7d83186d463dd15a481)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ImmersionKit",
-            url: "https://github.com/ImmersionNeuro/ImmersionKit/releases/download/v0.0.67/ImmersionKit.xcframework.zip",
-            checksum: "2ea97b9ed2edf98ef42aa76c927d480e1c1a0b53506b7c02ddb6352d5571fbbe"
+            url: "https://github.com/ImmersionNeuro/ImmersionKit/releases/download/v0.0.68/ImmersionKit.xcframework.zip",
+            checksum: "0ebc5d911623e9259d929bdd03b66b491e0e7a6c878fa7d83186d463dd15a481"
         )
     ]
 )
